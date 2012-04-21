9cac1933a9edcc543e78ff0a7e6c480b2ccb6cfe
Irfan Durmus <irfandurmus@gmail.com>
2012-04-21 18:38:16 +0300

    reason of could not new line

--- developers-prompt.sh
+++ developers-prompt.sh
@@ -75,5 +75,5 @@ function pretty_prompt () {
 
 
 PROMPT_COMMAND=pretty_prompt;
-export PS1="\e[32m\$DIR\e[33m\$BRANCH \e[32m\$\e[m "
+export PS1="\[\e[32;1m\]\$DIR\[\e[33;1m\]\$BRANCH\[\e[0m\]\[\e[32;1m\] \$ \[\e[0m\]\[\e[0m\]"
 
